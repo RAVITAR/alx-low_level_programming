e61faea68403c9303a6912228b94d6ead2ee377b
Mustapha <sannio.mustapha3@gmail.com>
2022-09-09 16:21:39 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = '0';
+	char ravi = 'a';
+
+	for (num = '0'; num <= '9'; ravi++)
+		putchar(num);
+	for (ravi = 'a'; ravi <= 'f'; ravi++)
+		putchar(ravi);
+
+	putchar('\n');
+	return (0);
+}
