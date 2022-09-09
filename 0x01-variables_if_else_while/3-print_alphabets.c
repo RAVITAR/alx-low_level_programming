41a113c565b4256780d49fefacc81a5d12235906
Mustapha <sannio.mustapha3@gmail.com>
2022-09-09 14:55:31 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ravi = 'a';
+	char Ravitar = 'A';
+
+	for (alpha = 'a'; ravi <= 'z'; ravi++)
+		putchar(ravi);
+	for (ravitar = 'A'; ravitar <= 'Z'; ravitar++)
+		putchar(ravitar);
+
+	putchar('\n');
+
+	return (0);
