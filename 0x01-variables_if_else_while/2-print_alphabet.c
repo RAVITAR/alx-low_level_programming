dce032b13f2651a0b9adace62aa8cd6754a81f8f
Mustapha <sannio.mustapha3@gmail.com>
2022-09-09 14:23:37 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ravi = 'a';
+
+	for (ravi = 'a'; ravi <= 'z'; ravi++)
+		putchar(ravi)
+
+	putchar('\n');
+
+	return (0);
+}
